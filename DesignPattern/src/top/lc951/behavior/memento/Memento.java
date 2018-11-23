1b5e5c63a228844e4a6974984a364c8994832e65
lichong <lichong@lichongdeMacBook-Pro-3.local>
2018-11-23 18:58:08 +0800

    备忘录模式

---
+++ DesignPattern/src/top/lc951/behavior/memento/Memento.java
@@ -0,0 +1,13 @@
+package top.lc951.behavior.memento;
+
+public class Memento {
+	 private String state;
+	 
+	   public Memento(String state){
+	      this.state = state;
+	   }
+	 
+	   public String getState(){
+	      return state;
+	   }  
+}
