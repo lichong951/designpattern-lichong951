6be30c851f1beaec2b4ad0156f3140e49ae23731
lichong <lichong@lichongdeMacBook-Pro-3.local>
2018-12-12 17:43:30 +0800

    解释器模式

---
+++ DesignPattern/src/top/lc951/behavior/interpreter/Expression.java
@@ -0,0 +1,5 @@
+package top.lc951.behavior.interpreter;
+
+public interface Expression {
+	public boolean interpret(String context);
+}
---
+++ DesignPattern/src/top/lc951/behavior/interpreter/TerminalExpression.java
@@ -0,0 +1,18 @@
+package top.lc951.behavior.interpreter;
+
+public class TerminalExpression implements Expression {
+
+	private String data;
+	 
+	   public TerminalExpression(String data){
+	      this.data = data; 
+	   }
+	 
+	   @Override
+	   public boolean interpret(String context) {
+	      if(context.contains(data)){
+	         return true;
+	      }
+	      return false;
+	   }
+}
