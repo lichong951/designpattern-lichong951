42f168d6820c3ef2d43eb606cf29b1f67a701e4c
lichong <lichong@lichongdeMacBook-Pro-3.local>
2018-12-13 18:28:15 +0800

    组合实体模式

---
+++ DesignPattern/src/top/lc951/cep/DependentObject2.java
@@ -0,0 +1,13 @@
+package top.lc951.cep;
+
+public class DependentObject2 {
+	private String data;
+	 
+	   public void setData(String data){
+	      this.data = data; 
+	   } 
+	 
+	   public String getData(){
+	      return data;
+	   }
+}
