64434a5a9a8ebcb8225b2aeef590c277a75fedb0
lichong <lichong@lichongdeMacBook-Pro-3.local>
2018-12-13 10:08:17 +0800

    模板模式

---
+++ DesignPattern/src/top/lc951/behavior/template/Football.java
@@ -0,0 +1,23 @@
+package top.lc951.behavior.template;
+
+public class Football extends Game {
+
+	@Override
+	void initialize() {
+		// TODO Auto-generated method stub
+		System.out.println("Football Game Initialized! Start playing.");
+	}
+
+	@Override
+	void startPlay() {
+		// TODO Auto-generated method stub
+		System.out.println("Football Game Started. Enjoy the game!");
+	}
+
+	@Override
+	void endPlay() {
+		// TODO Auto-generated method stub
+		System.out.println("Football Game Finished!");
+	}
+
+}
