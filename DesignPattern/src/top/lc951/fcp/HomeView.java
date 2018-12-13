005b84f17d49e4942ec875e91156831c2ea5a122
lichong <lichong@lichongdeMacBook-Pro-3.local>
2018-12-13 18:35:37 +0800

    前端控制器模式

---
+++ DesignPattern/src/top/lc951/fcp/HomeView.java
@@ -0,0 +1,7 @@
+package top.lc951.fcp;
+
+public class HomeView {
+	 public void show(){
+	      System.out.println("Displaying Home Page");
+	   }
+}
---
+++ DesignPattern/src/top/lc951/fcp/StudentView.java
@@ -0,0 +1,7 @@
+package top.lc951.fcp;
+
+public class StudentView {
+	public void show(){
+	      System.out.println("Displaying Student Page");
+	   }
+}
